cdfd18a0a8874436e6bc68d8dfa03abace441c50
Mamadou CONDE <mamadouconde15@yahoo.fr>
2025-09-16 13:58:39 +0200

    feat(mvp-must-have): add openapi for more documentation and manual test over the swagger

--- src/main/java/com/maconde/controller/OwnershipController.java
+++ src/main/java/com/maconde/controller/OwnershipController.java
@@ -3,6 +3,9 @@ package com.maconde.controller;
 import com.maconde.dto.StakeholderParticipationDto;
 import com.maconde.enums.StakeholderParticipationFilter;
 import com.maconde.service.OwnershipService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +24,15 @@ public class OwnershipController {
     }
 
     @GetMapping("/beneficiaries")
+    @Operation(
+            summary = "Get beneficiaries for a company",
+            description = "Returns the list of beneficiaries for the given company and filter"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of beneficial owners found"),
+            @ApiResponse(responseCode = "204", description = "No beneficial owner found"),
+            @ApiResponse(responseCode = "404", description = "Company not found")
+    })
     public ResponseEntity<List<StakeholderParticipationDto>> getBeneficiaries(
             @RequestParam Long companyId,
             @RequestParam StakeholderParticipationFilter filter) {
